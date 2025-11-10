9c89634a181f9404ad3bc6e40a1466aa1e5134b6
Willow (GHOST) <git@willow.sh>
2025-11-10 20:00:00 +0000

    fix: print can handle two adjacent tag nodes with missing whitespace

---
+++ .changeset/violet-terms-push.md
@@ -0,0 +1,5 @@
+---
+'stql': patch
+---
+
+fix: print can handle two adjacent tag nodes with missing whitespace
--- packages/stql/src/print.ts
+++ packages/stql/src/print.ts
@@ -1,13 +1,42 @@
-import type { TextNode, ParseResult } from './parse';
+import { type TextNode, type ParseResult, WHITESPACE } from './parse';
 
 function printText(text: TextNode) {
 	return text.quoted ? `"${text.value}"` : text.value;
 }
 
+function startsWithWhitespace(str: string) {
+	for (const char of WHITESPACE) {
+		if (str.startsWith(char)) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+function endsWithWhitespace(str: string) {
+	for (const char of WHITESPACE) {
+		if (str.endsWith(char)) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
 export function print(result: ParseResult) {
-	return result.nodes.reduce(
-		(result, node) =>
-			`${result}${node.type === 'text' ? printText(node) : `${node.key.value}:${printText(node.value)}`}`,
-		'',
-	);
+	return result.nodes.reduce((result, node, index, nodes) => {
+		const previousNode = index === 0 ? null : nodes.at(index - 1);
+
+		const requiresExtraWhitespace =
+			(node.type === 'text' &&
+				previousNode?.type === 'tag' &&
+				!startsWithWhitespace(node.value)) ||
+			(node.type === 'tag' && previousNode?.type === 'tag') ||
+			(node.type === 'tag' &&
+				previousNode?.type === 'text' &&
+				!endsWithWhitespace(previousNode.value));
+
+		return `${result}${requiresExtraWhitespace ? ' ' : ''}${node.type === 'text' ? printText(node) : `${node.key.value}:${printText(node.value)}`}`;
+	}, '');
 }
