f049225dbe5932044cf00721be6bcb4cf119b135
Willow (GHOST) <git@willow.sh>
2025-11-10 20:28:00 +0000

    fix: export node, tagnode, textnode, and parseresult

---
+++ .changeset/yellow-dryers-build.md
@@ -0,0 +1,5 @@
+---
+'stql': patch
+---
+
+fix: export `Node`, `TagNode`, `TextNode`, and `ParseResult`
--- packages/stql/src/index.ts
+++ packages/stql/src/index.ts
@@ -1,3 +1,4 @@
 export { parse } from './parse';
+export type { Node, TagNode, TextNode, ParseResult } from './parse';
 export { print } from './print';
 export { createTagNode, createTextNode } from './nodes';
