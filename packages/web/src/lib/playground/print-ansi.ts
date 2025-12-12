63eb07bc7bc167f9b0c95f1acdd50a9d9291eb34
Willow (GHOST) <git@willow.sh>
2025-12-12 22:49:26 +0000

    feat: better playground terminal line breaking

--- packages/web/src/lib/playground/print-ansi.ts
+++ packages/web/src/lib/playground/print-ansi.ts
@@ -5,5 +5,5 @@ inspect.styles.boolean = 'magenta';
 inspect.styles.string = 'yellow';
 
 export function printAnsi(thing: unknown) {
-	return inspect(thing, { depth: null, colors: true });
+	return inspect(thing, { depth: null, colors: true, breakLength: 32 });
 }
